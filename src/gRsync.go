47c4896c23a343f413d6e9790945096191837719
sujiahong <1511877962@qq.com>
2019-11-30 17:17:00 +0800

    文件锁

--- src/gRsync.go
+++ src/gRsync.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"bytes"
+	"fmt"
+	"os"
 	"os/exec"
+	"syscall"
+	"time"
 )
 
 func doSync(address string, srcDir string) error {
@@ -21,7 +25,65 @@ func doSync(address string, srcDir string) error {
 	return nil
 }
 
+func fileLock(f *os.File) {
+	// var fd = f.Fd()
+	// err := syscall.Flock(int(fd), syscall.LOCK_EX|syscall.LOCK_NB)
+	// if err != nil {
+	// 	fmt.Println("cannot lock file:  ", err)
+	// 	return
+	// }
+	// return
+	h, err := syscall.LoadLibrary("kernel32.dll")
+	if err != nil {
+		fmt.Println("err == ", err)
+		return
+	}
+	defer syscall.FreeLibrary(h)
+	addr, err := syscall.GetProcAddress(h, "LockFile")
+	if err != nil {
+		fmt.Println("22222  err == ", err)
+		return
+	}
+	r1, r2, err := syscall.Syscall6(addr, 5, f.Fd(), 0, 0, 0, 1, 0)
+	fmt.Println("rrr == ", r1, r2, err)
+}
+
+func fileUnlock(f *os.File) {
+	h, err := syscall.LoadLibrary("kernel32.dll")
+	if err != nil {
+		fmt.Println("err == ", err)
+		return
+	}
+	defer syscall.FreeLibrary(h)
+	addr, err := syscall.GetProcAddress(h, "UnlockFile")
+	if err != nil {
+		fmt.Println("22222  err == ", err)
+		return
+	}
+	r1, r2, err := syscall.Syscall6(addr, 5, f.Fd(), 0, 0, 0, 1, 0)
+	fmt.Println("rrr == ", r1, r2, err)
+}
+
 func main() {
-	err := doSync("10.0.1.210", "a")
-	println("error: ", err)
+	// err := doSync("10.0.1.210", "a")
+	// println("error: ", err)
+	f, err := os.Open("../file.lock")
+	if err != nil {
+		fmt.Println("eee: ", err)
+		return
+	}
+	fileLock(f)
+	//fileLock(f)
+	fileUnlock(f)
+	go func() {
+		f, err := os.Open("../file.lock")
+		if err != nil {
+			fmt.Println("eee: ", err)
+			return
+		}
+		var buf = make([]byte, 5)
+		n, err := f.Read(buf)
+		fmt.Println("n == ", n, err, buf)
+	}()
+	time.Sleep(2000 * time.Millisecond)
 }
